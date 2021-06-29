fd97f31d72ac1acefbc2ce2147410544ddbf5511
Juma Yunus <zekgyn@gmail.com>
2021-06-29 10:01:50 +0300

    init done

---
+++ resources/views/email.blade.php
@@ -0,0 +1,12 @@
+<h2>Hello User,</h2>
+<h3>Email received from: {{ $name }}</h3>
+
+<h4>User information:</h4>
+
+<p>Name: {{ $name }}</p>
+<p>Email: {{ $email }}</p>
+<p>Phone: {{ $phone }}</p>
+<p>Subject: {{ $subject }}</p>
+<p>Message: {{ $form_message }}</p>
+
+Thank you.
